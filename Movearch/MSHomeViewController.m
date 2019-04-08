2f3b4dd785bb45dac27525efc0d58586582ce5e0
Kaylen Travis Pillay <kaylen.pillay@takealot.com>
2019-04-08 08:20:16 +0200

    Add the home view and controllers

---
+++ Movearch/MSHomeViewController.m
@@ -0,0 +1,32 @@
+//
+//  MSHomeViewController.m
+//  Movearch
+//
+//  Created by Travis Pillay on 2019/04/08.
+//  Copyright © 2019 Takealot Grad Team. All rights reserved.
+//
+
+#import "MSHomeViewController.h"
+
+@interface MSHomeViewController ()
+
+@end
+
+@implementation MSHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
