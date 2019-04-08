2fde8cb9da35e467a1751cf9d0d99377b660b75a
Kaylen Travis Pillay <kaylen.pillay@takealot.com>
2019-04-08 09:05:16 +0200

    Added the search bar in the home navigation bar

--- Movearch/AppDelegate.m
+++ Movearch/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MSHomeViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // Create an instance of the MSHomeViewController
+    MSHomeViewController *homeViewController = [[MSHomeViewController alloc] init];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:homeViewController];
+    
+    self.window.rootViewController = navigationController;
     return YES;
 }
 
