805bd009da6257afa4777338f765ef8aa3ad5522
Kaylen Travis Pillay <kaylen.pillay@takealot.com>
2019-04-10 14:06:36 +0200

    Added caching for searchs to speed up app

--- Movearch/MSMovieItemStore.h
+++ Movearch/MSMovieItemStore.h
@@ -22,6 +22,8 @@ NS_ASSUME_NONNULL_BEGIN
                                      type:(NSString *) type
                                 posterURL:(NSString *) posterURL;
 
+- (void)restoreStore:(NSArray *)prevStore;
+
 - (void)clear;
 
 
